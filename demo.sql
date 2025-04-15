48e2a08219e105cf6faa6f1f226213006e826c31
Risi01 <157254960+Risi01@users.noreply.github.com>
2025-04-15 16:21:07 -0400

    Create demo.sql

---
+++ demo.sql
@@ -0,0 +1,4 @@
+CALL add_player('Martin Ødegaard', '1998-12-17', 'Midfielder', 6);
+CALL update_player(1, 'Jude Bellingham Jr.', '2003-06-29', 'Midfielder', 1);
+CALL delete_player(1);
+SELECT * FROM players_log;
